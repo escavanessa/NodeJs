1ae382a484edc0104ef5707fefc28d7010c90827
vanessa <escavanessa@gmail.com>
2024-04-03 18:53:58 -0400

    rock paper scissors!

---
+++ rps.js
@@ -0,0 +1,33 @@
+function getComputerChoice() {
+    //random choice bewteen rock, paper, and scissor
+    let arr = ['rock', 'paper', 'scissors'];
+    let randomNum = Math.floor(Math.random() * 3);
+    let choice = arr[randomNum]
+    return choice
+}
+
+
+let computerSelection = getComputerChoice();
+let playerSelection = 'rock'
+
+function playRound(computerSelection, playerSelection) {
+    if (computerSelection === playerSelection) {
+        console.log('its a tie!')
+    } else if (playerSelection === 'rock') {
+        if(computerSelection === 'scissors') {
+            console.log('rock beats scissors: player wins')
+        }
+    } else if (playerSelection === "paper") {
+        if(computerSelection === 'rock') {
+            console.log('paper beats rock: player wins')
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'paper') {
+            console.log('scissors beats paper: player wins')
+        }
+    } else {
+        console.log(playerSelection, computerSelection)
+    }
+}
+console.log(playerSelection, computerSelection)
+playRound()
