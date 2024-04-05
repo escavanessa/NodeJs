89b2c7428499918b3960dabd5dd4651178450ea0
vanessa <escavanessa@gmail.com>
2024-04-04 20:29:31 -0400

    got it working. i want to add a score board

--- rps.js
+++ rps.js
@@ -1,3 +1,8 @@
+//some serious discussions about scope in this project. in the playRound funciton, those parameters are just temporary in a sense.
+//they are created when the function runs - they can really be named anything. what matters is the parameters passed when the function is 
+//called. (? im still really confused ont this...???)
+for (let i = 1; i < 6; i++) {
+
 function getComputerChoice() {
     //random choice bewteen rock, paper, and scissor
     let arr = ['rock', 'paper', 'scissors'];
@@ -6,34 +11,59 @@ function getComputerChoice() {
     return choice
 }
 
+function getPlayerChoice() {
+    let arr = ['rock', 'paper', 'scissors'];
+    let randomNum = Math.floor(Math.random() * 3);
+    let choice = arr[randomNum]
+    return choice
+}
 
-let computerSelection = getComputerChoice();
-let playerSelection = 'rock'
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You win! Rock beats scissors.";
+    }
 
-function playRound(computerSelection, playerSelection) {
-    if (computerSelection === playerSelection) {
-        console.log('its a tie!')
-    } else if (playerSelection === 'rock') {
-        if(computerSelection === 'scissors') {
-            console.log('rock beats scissors: player wins')
-        } else {
-            console.log('comp wins')
-        }
-    } else if (playerSelection === "paper") {
-        if(computerSelection === 'rock') {
-            console.log('paper beats rock: player wins')
-        } else {
-            console.log('comp wins')
-        }
-    } else if (playerSelection === 'scissors') {
-        if (computerSelection === 'paper') {
-            console.log('scissors beats paper: player wins')
-        } else {
-            console.log('comp wins')
-        }
-    } else {
-       return
+    if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You win! Paper beats rock.";
+    }
+
+    if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You win! Scissors beats paper.";
+    }
+
+    // Computer Case
+    if (computerSelection == "rock" && playerSelection == "scissors") {
+        return "You lose! Scissors loses to rock.";
+    }
+
+    if (computerSelection == "paper" && playerSelection == "rock") {
+        return "You lose! Rock loses to paper.";
+    }
+
+    if (computerSelection == "scissors" && playerSelection == "paper") {
+        return "You lose! Paper loses to scissors.";
+    }
+
+    // Tie case
+    if (playerSelection == "rock" && computerSelection == "rock") {
+        return "Tie!";
+    }
+
+    if (playerSelection == "paper" && computerSelection == "paper") {
+        return "Tie!";
+    }
+
+    if (playerSelection == "scissors" && computerSelection == "scissors") {
+        return "Tie!";
     }
 }
-console.log(playerSelection, computerSelection)
-playRound(playerSelection, computerSelection)
+
+let playerSelection = getPlayerChoice();
+let computerSelection = getComputerChoice();
+
+console.log('Round:' + i)
+console.log("Player Selection: " + playerSelection)
+console.log("Computer Selection: " + computerSelection)
+console.log(playRound(playerSelection, computerSelection))
+
+}
