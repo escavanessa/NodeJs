ade3fcad7dbd513c06c1d090ac92822a1b5d3900
vanessa <escavanessa@gmail.com>
2025-01-19 14:05:27 -0500

    adding button logic

--- jssim/rps.js
+++ jssim/rps.js
@@ -1,72 +1,100 @@
 
 
-const btn1 = document.querySelector("#rock");
-btn1.addEventListener("click", () => {
-    console.log("hello")
-})
-
-const btn2 = document.querySelector("#paper");
-btn2.addEventListener("click", () => {
-    console.log("hello")
-})
-
-const btn3 = document.querySelector("#scissor");
-btn3.addEventListener("click", () => {
-    console.log("hello")
-})
-
-
+const rockButton = document.querySelector("#rock");
+// btn1.addEventListener("click", () => {
+//     console.log("hello")
+// })
+const paperButton = document.querySelector("#paper");
+// btn2.addEventListener("click", () => {
+//     console.log("hello")
+// })
+const scissorButton = document.querySelector("#scissor");
+// btn3.addEventListener("click", () => {
+//     console.log("hello")
+// })
+const results = document.querySelector("#result-wrapper");
 //function humanSelection() {} maybe dont ened it because of the buttons?
 
 
-function computerSelection() {
+function computerPlay() {
     let choices = ['rock', 'paper', 'scissors'];
     let num = Math.floor(Math.random() * 3);
     let choice = choices[num];
     return choice
 }
 
-function playRound() {
+function playRound(playerSelection, computerSelection) {  //remember params are just place holders, they dont really mean anything
     if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "You win! Rock beats scissors.";
+        const p = document.createElement('p')
+        p.innerText = 'You win! Rock beats scissors.'
     }
 
     if (playerSelection == "paper" && computerSelection == "rock") {
-        return "You win! Paper beats rock.";
+        const p = document.createElement('p')
+        p.innerText = 'You win! Paper beats rock.'
     }
 
     if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "You win! Scissors beats paper.";
+        const p = document.createElement('p')
+        p.innerText = 'You win! Scissors beats paper.'
     }
 
     // Computer Case
     if (computerSelection == "rock" && playerSelection == "scissors") {
-        return "You lose! Scissors loses to rock.";
+        const p = document.createElement('p')
+        p.innerText = 'You lose! Scissors loses to rock.'
     }
 
     if (computerSelection == "paper" && playerSelection == "rock") {
-        return "You lose! Rock loses to paper.";
+        const p = document.createElement('p')
+        p.innerText = 'You lose! Rock loses to paper.'
     }
 
     if (computerSelection == "scissors" && playerSelection == "paper") {
-        return "You lose! Paper loses to scissors.";
+        const p = document.createElement('p')
+        p.innerText = 'You lose! Paper loses to scissors.'
     }
 
     // Tie case
     if (playerSelection == "rock" && computerSelection == "rock") {
-        return "Tie!";
+        const p = document.createElement('p')
+        p.innerText = 'Tie!'
     }
 
     if (playerSelection == "paper" && computerSelection == "paper") {
-        return "Tie!";
+        const p = document.createElement('p')
+        p.innerText = 'Tie!'
     }
 
     if (playerSelection == "scissors" && computerSelection == "scissors") {
-        return "Tie!";
+        const p = document.createElement('p')
+        p.innerText = 'Tie!'
     }
 }
 
-let computerChoice = computerSelection()
+
+rockButton.addEventListener('click', () => {
+    const computerSelection = computerPlay() //the random number
+    const playerSelection = "rock"
+    playRound(playerSelection, computerSelection)
+})
+
+paperButton.addEventListener('click', () => {
+    const computerSelection = computerPlay() //the random number
+    const playerSelection = "paper"
+    playRound(playerSelection, computerSelection)
+})
+
+
+scissorButton.addEventListener('click', () => {
+    const computerSelection = computerPlay() //the random number
+    const playerSelection = "scissor"
+    playRound(playerSelection, computerSelection)
+})
+
+
+let computerSelection = computerPlay() // if i made a game function to call the game like five times, this computer selection 
+//would need to go inside the loop so the computer selection changes every time the loop interates. if not the selection would remian the same.
 
 
 
